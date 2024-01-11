5498e03cb33e7832e75be5c2648e10ca8dfe72ec
Lucas Freitas <l.freittas@outlook.com>
2024-01-11 20:03:49 -0300

    feat: like count

--- src/Components/Comment/index.jsx
+++ src/Components/Comment/index.jsx
@@ -1,8 +1,15 @@
+import { useState } from 'react'
 import { Avatar } from '../Avatar'
 import { Container, Text, Actions, CommentWrapper } from './styles'
 import { Trash, ThumbsUp } from '@phosphor-icons/react'
 
 export function Comment({commentText, onDeleteComment}){
+
+    const [claps, setClaps] = useState(0)
+
+    function handleClapCounter(){
+        setClaps(claps + 1)
+    }
 
     function handleDeleteComment(){
         onDeleteComment(commentText)
@@ -28,11 +35,11 @@ export function Comment({commentText, onDeleteComment}){
                     
                 </Text>
             </CommentWrapper>
-            <Actions>
+            <Actions onClick={handleClapCounter}>
                 <div>
                     <ThumbsUp size={20}/> 
                     <p>Aplaudir</p>
-                    <span>•&nbsp;&nbsp;&nbsp;03</span>
+                    <span>•&nbsp;&nbsp;&nbsp;{claps}</span>
                 </div>
             </Actions>
             
